8d76d42cdea815b6f4b112ea2c1dcf6974fcd44d
EL-patata <youssiftharwat26@gmail.com>
2024-08-17 14:23:54 +0300

    fixing auth on deployment #2

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	webpack: (config) => {
-		config.externals.push('@node-rs/argon2', '@node-rs/bcrypt');
-		return config;
-	},
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
